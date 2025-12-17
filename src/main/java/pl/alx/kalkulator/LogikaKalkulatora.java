4c2ba4d97febf0b28eaa4cce737f76e8341da059
Grzegorz Orłowski <grzeslaw505@gmail.com>
2025-12-17 12:27:17 +0100

    add divide function"

--- src/main/java/pl/alx/kalkulator/LogikaKalkulatora.java
+++ src/main/java/pl/alx/kalkulator/LogikaKalkulatora.java
@@ -11,8 +11,14 @@ public class LogikaKalkulatora {
             case "+" -> liczba1 + liczba2;
             case "-" -> liczba1 - liczba2;
             case "*" -> liczba1 * liczba2;
+            case "/" -> dziel(liczba1,liczba2);
             default -> throw new IllegalArgumentException("Nieznana operacja " + operacja);
         };
     }
-
+    public long dziel(long l1, long l2)
+    {
+        if (l2==0)
+            throw new IllegalArgumentException("Nie dziel przez 0");
+        return l1/l2
+    }
 }
